fe28d65a10c19b369cba159fbf40318db5cab7d5
jose <jrodriguez@itsolutionspcllc.com>
2023-01-22 18:18:19 -0500

    DOM

---
+++ dompratice.js
@@ -0,0 +1,37 @@
+document.getElementById("container");
+
+
+
+document.querySelector("#container")
+
+
+document.querySelectorAll(".second")
+
+
+
+document.querySelector("ol .third");
+
+
+let div = document.querySelector("#container");
+div.innerText = "Hello";
+
+
+let footer1 = document.querySelector(".footer");
+footer1.classList.add("main");
+
+let newtagLi = document.createElement("li");
+
+newtagLiLi.innerText = "four"
+
+let list = document.querySelector("ul");
+list.appendChild(newLi);
+
+
+let liOl = document.querySelectorAll("ol li");
+for(let i = 0; i < liInsideOl.length; i++){
+liOl[i].style.backgroundColor = "green";
+}
+
+
+let footer = document.querySelector(".footer");
+footer.remove()
